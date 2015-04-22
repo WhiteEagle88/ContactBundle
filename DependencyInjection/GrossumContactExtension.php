bb8481d44a3a9b4b78f20a7fd878f00dc36c4fe6
Dmitro Boyko <dboyko@grossum.com>
2015-04-22 11:38:33 +0300

    Added gitignore

--- DependencyInjection/GrossumContactExtension.php
+++ DependencyInjection/GrossumContactExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -21,6 +22,10 @@ class GrossumContactExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        if (!class_exists('Grossum\CoreBundle\GrossumCoreBundle')) {
+            throw new LogicException('GrossumContactBundle required GrossumCoreBundle');
+        }
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
